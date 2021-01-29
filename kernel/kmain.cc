8cc59c4acf3c1506b01163d18a9e99ad74aff1f5
Owen Smith <yeeetari@gmail.com>
2021-01-29 19:18:43 +0000

    kernel: Use new log function

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -1,14 +1,17 @@
 #include <boot/BootInfo.hh>
 #include <kernel/Port.hh>
+#include <ustd/Log.hh>
 #include <ustd/Types.hh>
+
+void put_char(char ch) {
+    port_write(0xE9, ch);
+}
 
 extern "C" void kmain(BootInfo *boot_info) {
     if (port_read(0xE9) == 0xE9) {
-        // Write to QEMU debug port.
-        const char *msg = "Hello, world!\n\0";
-        for (; *msg != '\0'; msg++) {
-            port_write(0xE9, *msg);
-        }
+        log("Hello, world!");
+        log("boot_info = {}", boot_info);
+        log("framebuffer = {:h}", boot_info->framebuffer_base);
     }
 
     // Clear screen.
