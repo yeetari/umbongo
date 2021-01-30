5a2a2accef02129aab511dc99d0372f1a2b32127
Owen Smith <yeeetari@gmail.com>
2021-01-30 15:17:43 +0000

    kernel: Implement assertion_failed

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -1,7 +1,17 @@
 #include <boot/BootInfo.hh>
 #include <kernel/Port.hh>
+#include <ustd/Assert.hh>
 #include <ustd/Log.hh>
 #include <ustd/Types.hh>
+
+[[noreturn]] void assertion_failed(const char *file, unsigned int line, const char *expr, const char *msg) {
+    log("Assertion '{}' failed at {}:{}", expr, file, line);
+    log("=> {}", msg);
+    while (true) {
+        asm volatile("cli");
+        asm volatile("hlt");
+    }
+}
 
 void put_char(char ch) {
     port_write(0xE9, ch);
@@ -16,10 +26,10 @@ extern "C" void kmain(BootInfo *boot_info) {
 
     // Clear screen.
     const uint64 bytes_per_scan_line = boot_info->pixels_per_scan_line * sizeof(uint32);
-    for (uint32 y = 0; y < boot_info->height; y++) {
+    for (uint32 y = boot_info->height / 2; y < boot_info->height; y++) {
         auto *pixel = reinterpret_cast<uint32 *>(boot_info->framebuffer_base + (bytes_per_scan_line * y));
         for (uint32 x = 0; x < boot_info->width; x++) {
-            *pixel++ = 0;
+            *pixel++ = 255;
         }
     }
 }
