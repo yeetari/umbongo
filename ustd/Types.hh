50b718c70753eb2677c47b87cd491b1de34ce7ee
Owen Smith <yeeetari@gmail.com>
2021-01-30 15:15:07 +0000

    ustd: Add signed integer types

--- ustd/Types.hh
+++ ustd/Types.hh
@@ -1,5 +1,9 @@
 #pragma once
 
+using int8 = __INT8_TYPE__;
+using int16 = __INT16_TYPE__;
+using int32 = __INT32_TYPE__;
+using int64 = __INT64_TYPE__;
 using ptrdiff = __PTRDIFF_TYPE__;
 using uint8 = __UINT8_TYPE__;
 using uint16 = __UINT16_TYPE__;
