482d9ebba3bf39dc90ef1a49b6071fb3419f0f1b
Owen Smith <yeeetari@gmail.com>
2021-02-04 22:38:58 +0000

    kernel: Print some memory info

--- kernel/kmain.cc
+++ kernel/kmain.cc
@@ -21,15 +21,29 @@ void put_char(char ch) {
 
 extern "C" void kmain(BootInfo *boot_info) {
     if constexpr (KERNEL_STACK_PROTECTOR) {
-        log("SSP initialised with guard value {:h}", __stack_chk_guard);
+        log("core: SSP initialised with guard value {:h}", __stack_chk_guard);
     }
     if constexpr (KERNEL_QEMU_DEBUG) {
         ENSURE(port_read(0xE9) == 0xE9, "KERNEL_QEMU_DEBUG config option enabled, but port E9 isn't available!");
     }
 
-    log("Hello, world!");
-    log("boot_info = {}", boot_info);
-    log("framebuffer = {:h}", boot_info->framebuffer_base);
+    log("core: boot_info = {}", boot_info);
+    log("core: framebuffer = {:h}", boot_info->framebuffer_base);
+
+    usize total_mem = 0;
+    usize usable_mem = 0;
+    for (usize i = 0; i < boot_info->map_entry_count; i++) {
+        auto *entry = &boot_info->map[i];
+        ENSURE(entry->base % 4096 == 0);
+
+        const usize entry_size = entry->page_count * 4096;
+        total_mem += entry_size;
+        if (entry->type != MemoryType::Reserved) {
+            usable_mem += entry_size;
+        }
+    }
+    log(" mem: {}MiB/{}MiB free ({}%)", usable_mem / 1024 / 1024, total_mem / 1024 / 1024,
+        (usable_mem * 100) / total_mem);
 
     // Clear screen.
     const uint64 bytes_per_scan_line = boot_info->pixels_per_scan_line * sizeof(uint32);
