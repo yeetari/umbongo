8abbb0c73841434d4378fa747e1570bc5adea7d6
Owen Smith <yeeetari@gmail.com>
2021-02-15 16:31:31 +0000

    ustd: Fix lost const-qualifier issue in Array

--- ustd/Array.hh
+++ ustd/Array.hh
@@ -12,7 +12,7 @@ struct Array {
     constexpr const T &operator[](usize index) const { return m_data[index]; }
 
     constexpr T *data() { return static_cast<T *>(m_data); }
-    constexpr const T *data() const { return static_cast<T *>(m_data); }
+    constexpr const T *data() const { return static_cast<const T *>(m_data); }
 
     constexpr usize size() const { return N; }
     constexpr usize size_in_bytes() const { return N * sizeof(T); }
