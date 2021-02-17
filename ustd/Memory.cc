851ce2a9097ccf760e6ceeb8cf60157ae3c61b4c
Owen Smith <yeeetari@gmail.com>
2021-02-17 21:35:54 +0000

    ustd: Implement memcmp

--- ustd/Memory.cc
+++ ustd/Memory.cc
@@ -2,6 +2,20 @@
 
 #include <ustd/Types.hh>
 
+extern "C" int memcmp(const void *aptr, const void *bptr, usize size) {
+    const auto *a = static_cast<const unsigned char *>(aptr);
+    const auto *b = static_cast<const unsigned char *>(bptr);
+    for (usize i = 0; i < size; i++) {
+        if (a[i] < b[i]) {
+            return -1;
+        }
+        if (b[i] < a[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 extern "C" void *memset(void *bufptr, int value, usize size) {
     auto *buf = static_cast<unsigned char *>(bufptr);
     for (usize i = 0; i < size; i++) {
--- ustd/Memory.hh
+++ ustd/Memory.hh
@@ -2,6 +2,7 @@
 
 #include <ustd/Types.hh>
 
+extern "C" int memcmp(const void *, const void *, usize);
 extern "C" void *memset(void *, int, usize);
 
 inline void *operator new(usize, void *ptr) {
