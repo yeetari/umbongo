d5545988efb4def5bdc0cdbd635e3ac1423ea67f
Owen Smith <yeeetari@gmail.com>
2021-03-06 12:16:44 +0000

    kernel: Allow 2 and 4 byte port writes

--- kernel/Port.hh
+++ kernel/Port.hh
@@ -3,13 +3,13 @@
 #include <ustd/Types.hh>
 
 template <typename T = uint8>
-T port_read(uint16 port) requires(sizeof(T) == 1) {
+T port_read(uint16 port) requires(sizeof(T) <= 4) {
     T value;
     asm volatile("in %1, %0" : "=a"(value) : "Nd"(port));
     return value;
 }
 
 template <typename T>
-void port_write(uint16 port, T value) requires(sizeof(T) == 1) {
+void port_write(uint16 port, T value) requires(sizeof(T) <= 4) {
     asm volatile("out %0, %1" : : "a"(value), "Nd"(port));
 }
