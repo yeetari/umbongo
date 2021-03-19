b8efab53c88cdd09663d341cdca2399cb57f517a
Owen Smith <yeeetari@gmail.com>
2021-03-19 19:41:32 +0000

    ustd: Prefer uint8 to unsigned char

--- ustd/Memory.cc
+++ ustd/Memory.cc
@@ -3,8 +3,8 @@
 #include <ustd/Types.hh>
 
 extern "C" int memcmp(const void *aptr, const void *bptr, usize size) {
-    const auto *a = static_cast<const unsigned char *>(aptr);
-    const auto *b = static_cast<const unsigned char *>(bptr);
+    const auto *a = static_cast<const uint8 *>(aptr);
+    const auto *b = static_cast<const uint8 *>(bptr);
     for (usize i = 0; i < size; i++) {
         if (a[i] < b[i]) {
             return -1;
@@ -17,8 +17,8 @@ extern "C" int memcmp(const void *aptr, const void *bptr, usize size) {
 }
 
 extern "C" void *memcpy(void *dstptr, const void *srcptr, usize size) {
-    auto *dst = static_cast<unsigned char *>(dstptr);
-    const auto *src = static_cast<const unsigned char *>(srcptr);
+    auto *dst = static_cast<uint8 *>(dstptr);
+    const auto *src = static_cast<const uint8 *>(srcptr);
     for (usize i = 0; i < size; i++) {
         dst[i] = src[i];
     }
@@ -26,9 +26,9 @@ extern "C" void *memcpy(void *dstptr, const void *srcptr, usize size) {
 }
 
 extern "C" void *memset(void *bufptr, int value, usize size) {
-    auto *buf = static_cast<unsigned char *>(bufptr);
+    auto *buf = static_cast<uint8 *>(bufptr);
     for (usize i = 0; i < size; i++) {
-        buf[i] = static_cast<unsigned char>(value);
+        buf[i] = static_cast<uint8>(value);
     }
     return bufptr;
 }
@@ -39,7 +39,7 @@ extern "C" int strcmp(const char *s1, const char *s2) {
             return 0;
         }
     }
-    return *reinterpret_cast<const unsigned char *>(s1) - *reinterpret_cast<const unsigned char *>(--s2);
+    return *reinterpret_cast<const uint8 *>(s1) - *reinterpret_cast<const uint8 *>(--s2);
 }
 
 extern "C" usize strlen(const char *str) {
