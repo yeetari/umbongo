114d5f449b724efd41748cbe43d42cb2ca45ebf4
Owen Smith <yeeetari@gmail.com>
2021-03-21 19:18:10 +0000

    kernel: Assume all memory is reserved until proven otherwise

--- kernel/mem/MemoryManager.cc
+++ kernel/mem/MemoryManager.cc
@@ -43,14 +43,17 @@ MemoryManager::MemoryManager(BootInfo *boot_info) : m_boot_info(boot_info) {
     ENSURE(bitset_location);
 
     // Construct the frame bitset and then set reserved regions from the memory map.
-    m_frame_bitset = new (reinterpret_cast<void *>(*bitset_location)) usize[bucket_count]{};
+    m_frame_bitset = new (reinterpret_cast<void *>(*bitset_location)) usize[bucket_count];
+    for (usize i = 0; i < m_total_frames; i++) {
+        set_frame(i);
+    }
     for (usize i = 0; i < boot_info->map_entry_count; i++) {
         auto &entry = boot_info->map[i];
-        if (entry.type == MemoryType::Available) {
+        if (entry.type != MemoryType::Available) {
             continue;
         }
         for (usize j = 0; j < entry.page_count; j++) {
-            set_frame(entry.base / k_frame_size + j);
+            clear_frame(entry.base / k_frame_size + j);
         }
     }
 
