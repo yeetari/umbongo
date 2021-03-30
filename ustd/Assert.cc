bb65d988af67a72c140fce6cadab85f3e46b94fd
Owen Smith <yeeetari@gmail.com>
2021-03-30 18:36:10 +0100

    ustd: Disable interrupts at the top of the assertion failed handler

--- ustd/Assert.cc
+++ ustd/Assert.cc
@@ -3,11 +3,12 @@
 #include <ustd/Log.hh>
 
 [[noreturn]] void assertion_failed(const char *file, unsigned int line, const char *expr, const char *msg) {
+    asm volatile("cli");
     logln("\nAssertion '{}' failed at {}:{}", expr, file, line);
     if (msg != nullptr) {
         logln("=> {}", msg);
     }
     while (true) {
-        asm volatile("cli; hlt");
+        asm volatile("hlt");
     }
 }
