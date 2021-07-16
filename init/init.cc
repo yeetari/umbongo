b32d1272dffab5a404f03f289469ff72dcac727b
Owen Smith <yeeetari@gmail.com>
2021-07-16 23:53:10 +0100

    init: Load and print hello world file

--- init/init.cc
+++ init/init.cc
@@ -1,4 +1,5 @@
 #include <kernel/Syscall.hh>
+#include <ustd/Array.hh>
 #include <ustd/Log.hh>
 #include <ustd/Types.hh>
 
@@ -6,8 +7,15 @@ void put_char(char ch) {
     Syscall::invoke(Syscall::putchar, ch);
 }
 
-extern "C" void main() {
+int main() {
     uint64 pid = Syscall::invoke(Syscall::getpid);
-    logln("Hello, world!");
-    Syscall::invoke(Syscall::exit, pid);
+    uint64 fd = Syscall::invoke(Syscall::open, "/home/hello");
+    Array<char, 20> data{'\0'};
+    Syscall::invoke(Syscall::read, fd, data.data(), data.size());
+    logln("[#{}]: {}", pid, static_cast<const char *>(data.data()));
+    return static_cast<int>(pid);
+}
+
+extern "C" void _start() {
+    Syscall::invoke(Syscall::exit, main());
 }
