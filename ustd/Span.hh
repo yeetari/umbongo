c28577360e411f9cd5188992da21aa133971742b
Owen Smith <yeeetari@gmail.com>
2021-07-28 20:54:08 +0100

    ustd: Add Span::as<T> conversion functions

--- ustd/Span.hh
+++ ustd/Span.hh
@@ -15,6 +15,15 @@ public:
     constexpr Span() = default;
     constexpr Span(T *data, usize size) : m_data(data), m_size(size) {}
 
+    template <typename U>
+    constexpr Span<U> as() {
+        return {static_cast<U *>(m_data), m_size};
+    }
+    template <typename U>
+    constexpr Span<const U> as() const {
+        return {static_cast<const U *>(m_data), m_size};
+    }
+
     // Allow implicit conversion of `Span<T>` to `Span<void>`.
     constexpr operator Span<void>() { return {data(), size_bytes()}; }
     constexpr operator Span<const void>() const { return {data(), size_bytes()}; }
