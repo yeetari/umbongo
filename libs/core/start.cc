35873ed3401805a9429e5f4ce4dc492b9c66f26b
Owen Smith <yeeetari@gmail.com>
2021-08-12 19:28:58 +0100

    libs/core: Align stack on entry

--- libs/core/start.cc
+++ libs/core/start.cc
@@ -4,5 +4,6 @@
 usize main(usize argc, const char **argv);
 
 extern "C" void _start(usize argc, const char **argv) {
+    asm volatile("andq $~15, %rsp");
     Syscall::invoke(Syscall::exit, main(argc, argv));
 }
