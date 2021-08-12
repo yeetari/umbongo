2ca98251e6fed7721001a326d7ead716aeb6dde4
Owen Smith <yeeetari@gmail.com>
2021-08-12 23:11:42 +0100

    sysroot: Create two assembly test files

--- sysroot/home/puts.asm
+++ sysroot/home/puts.asm
@@ -1,14 +1,13 @@
 bits 64
 
 section .rodata
-string:
-    db "Hello, world", 0
+string: db "Hello, world", 0
 
 section .text
 extern puts
 global main
 main:
-    mov rdi, string
+    lea rdi, [rel string]
     call puts
     xor eax, eax
     ret
---
+++ sysroot/home/syscall.asm
@@ -0,0 +1,20 @@
+bits 64
+
+SYS_EXIT  equ 5
+SYS_WRITE equ 17
+
+section .rodata
+string: db "Hello, world", 0x0a
+.len: equ $ - string
+
+section .text
+global _start
+_start:
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    lea rsi, [rel string]
+    mov rdx, string.len
+    syscall
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
