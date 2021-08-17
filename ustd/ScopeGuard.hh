a278337ae7e8d30923aa63732ba3a851a771abbf
Owen Smith <yeeetari@gmail.com>
2021-08-17 16:34:41 +0100

    ustd: Add ScopeGuard RAII callback class

---
+++ ustd/ScopeGuard.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <ustd/Utility.hh>
+
+namespace ustd {
+
+template <typename Callback>
+class ScopeGuard {
+    const Callback m_callback;
+
+public:
+    ScopeGuard(Callback callback) : m_callback(move(callback)) {}
+    ScopeGuard(const ScopeGuard &) = delete;
+    ScopeGuard(ScopeGuard &&) = delete;
+    ~ScopeGuard() { m_callback(); }
+
+    ScopeGuard &operator=(const ScopeGuard &) = delete;
+    ScopeGuard &operator=(ScopeGuard &&) = delete;
+};
+
+template <typename Callback>
+ScopeGuard(Callback) -> ScopeGuard<Callback>;
+
+} // namespace ustd
+
+using ustd::ScopeGuard;
