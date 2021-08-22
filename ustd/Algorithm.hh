43d2539923c6798082cd349fc003ae2799069478
Owen Smith <yeeetari@gmail.com>
2021-08-22 21:00:55 +0100

    ustd: Add sort algorithm function

--- ustd/Algorithm.hh
+++ ustd/Algorithm.hh
@@ -45,4 +45,37 @@ constexpr void fill(Container &container, const T &value) {
     }
 }
 
+template <Container Container, typename GreaterThan, typename SizeType = decltype(declval<Container>().size())>
+constexpr void sort(Container &container, GreaterThan gt) {
+    if (container.size() == 0) {
+        return;
+    }
+    auto gap = container.size();
+    bool swapped = false;
+    do {
+        gap = (gap * 10) / 13;
+        if (gap == 9 || gap == 10) {
+            gap = 11;
+        }
+        if (gap < 1) {
+            gap = 1;
+        }
+        swapped = false;
+        for (SizeType i = 0; i < container.size() - gap; i++) {
+            SizeType j = i + gap;
+            if (gt(container[i], container[j])) {
+                swap(container[i], container[j]);
+                swapped = true;
+            }
+        }
+    } while (gap > 1 || swapped);
+}
+
+template <Container Container>
+constexpr void sort(Container &container) {
+    sort(container, [](const auto &lhs, const auto &rhs) {
+        return lhs > rhs;
+    });
+}
+
 } // namespace ustd
