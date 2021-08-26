4ccb72e13c5edbb7720ab8fb033e8a64ef1689cc
Owen Smith <yeeetari@gmail.com>
2021-08-26 13:24:23 +0100

    ports: Add a tcc port

---
+++ ports/tcc/install.sh
@@ -0,0 +1,31 @@
+set -e
+
+if [ "$#" -ne 1 ]; then
+    printf "Usage: $0 <build-dir>\n"
+    exit 1
+fi
+
+SYSROOT=$(realpath $1/sysroot)
+cd $(dirname $0)
+
+DIRECTORY=tinycc
+
+if [ ! -d $DIRECTORY ]; then
+    git clone https://github.com/TinyCC/tinycc.git
+fi
+cd $DIRECTORY
+git reset --hard dda95e9b0b30771369efe66b4a47e94cf0ca7dc0
+for path in ../patches/*.patch; do
+    patch -p1 -N < "$path" || true
+done
+
+./configure \
+    --ar=llvm-ar \
+    --cc=clang \
+    --extra-cflags="--sysroot=$SYSROOT -fpic -DCONFIG_TCC_STATIC -DCONFIG_TCC_SEMLOCK=0" \
+    --extra-ldflags="--sysroot=$SYSROOT -fuse-ld=lld -nostdlib -pie -lcore -lposix -lustd -Xlinker -dynamic-linker -Xlinker /bin/dynamic-linker" \
+    --sysroot=$SYSROOT \
+    --elfinterp=/bin/dynamic-linker \
+    --sysincludepaths=/include
+make clean && make -j$(nproc) tcc
+cp tcc $SYSROOT/bin
