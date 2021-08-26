b93c90abb1d200c06bf752639f8cfe3d72785872
Owen Smith <yeeetari@gmail.com>
2021-08-26 13:23:05 +0100

    libs/posix: Implement open

--- libs/posix/fcntl.cc
+++ libs/posix/fcntl.cc
@@ -1,9 +1,13 @@
 #include <fcntl.h>
 
+#include <bits/error.hh>
+#include <errno.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <sys/cdefs.h>
 
+#include <kernel/Syscall.hh>
+#include <kernel/SyscallTypes.hh>
 #include <ustd/Assert.hh>
 #include <ustd/Log.hh>
 
@@ -21,8 +25,29 @@ int fcntl(int fd, int cmd, ...) {
     ENSURE_NOT_REACHED("Unknown fcntl");
 }
 
-int open(const char *, int, ...) {
-    ENSURE_NOT_REACHED();
+int open(const char *path, int oflag, ...) {
+    auto flags = static_cast<uint32_t>(oflag);
+    flags &= ~(O_RDONLY | O_WRONLY);
+
+    auto mode = OpenMode::None;
+    if ((flags & O_CREAT) == O_CREAT) {
+        mode |= OpenMode::Create;
+    }
+    if ((flags & O_TRUNC) == O_TRUNC) {
+        mode |= OpenMode::Truncate;
+    }
+    flags &= ~(O_CREAT | O_TRUNC);
+    if (flags != 0) {
+        dbgln("Unknown open flag bitset {:h}", flags);
+        ENSURE_NOT_REACHED();
+    }
+
+    auto rc = Syscall::invoke(Syscall::open, path, mode);
+    if (rc < 0) {
+        errno = posix::to_errno(rc);
+        return -1;
+    }
+    return static_cast<int>(rc);
 }
 
 __END_DECLS
