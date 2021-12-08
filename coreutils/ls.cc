268ff9190bca7883acfb7dfadf7b106b81504e24
Owen Smith <yeeetari@gmail.com>
2021-12-08 20:12:19 +0000

    coreutils: Use core::iterate_directory

--- coreutils/ls.cc
+++ coreutils/ls.cc
@@ -1,14 +1,15 @@
+#include <core/Directory.hh>
 #include <core/Error.hh>
-#include <kernel/Syscall.hh>
 #include <ustd/Algorithm.hh>
 #include <ustd/Log.hh>
 #include <ustd/Memory.hh>
 #include <ustd/Numeric.hh>
+#include <ustd/String.hh>
 #include <ustd/StringView.hh>
 #include <ustd/Types.hh>
 #include <ustd/Vector.hh>
 
-bool compare_name(ustd::StringView lhs, ustd::StringView rhs) {
+bool compare_name(ustd::String &lhs, ustd::String &rhs) {
     if (int rc = memcmp(lhs.data(), rhs.data(), ustd::min(lhs.length(), rhs.length()))) {
         return rc > 0;
     }
@@ -17,24 +18,17 @@ bool compare_name(ustd::StringView lhs, ustd::StringView rhs) {
 
 usize main(usize argc, const char **argv) {
     const char *path = argc == 1 ? "." : argv[1];
-    ssize rc = Syscall::invoke(Syscall::read_directory, path, nullptr);
+    ustd::Vector<ustd::String> names;
+    auto rc = core::iterate_directory(path, [&](ustd::StringView name) {
+        names.emplace(name);
+    });
     if (rc < 0) {
-        printf("ls: {}: {}\n", path, core::error_string(rc));
+        ustd::printf("ls: {}: {}\n", path, core::error_string(rc));
         return 1;
     }
-    auto byte_count = static_cast<usize>(rc);
-    ustd::LargeVector<char> data;
-    data.ensure_capacity(byte_count);
-    Syscall::invoke(Syscall::read_directory, path, data.data());
-    ustd::Vector<ustd::StringView> views;
-    for (usize byte_offset = 0; byte_offset != byte_count;) {
-        ustd::StringView view(data.data() + byte_offset);
-        views.push(view);
-        byte_offset += view.length() + 1;
-    }
-    ustd::sort(views, compare_name);
-    for (const auto &view : views) {
-        printf("{}\n", view);
+    ustd::sort(names, compare_name);
+    for (const auto &name : names) {
+        ustd::printf("{}\n", name.view());
     }
     return 0;
 }
--- coreutils/lspci.cc
+++ coreutils/lspci.cc
@@ -1,28 +1,29 @@
+#include <core/Directory.hh>
 #include <core/Error.hh>
 #include <core/File.hh>
-#include <kernel/Syscall.hh>
+#include <core/Process.hh>
 #include <kernel/SyscallTypes.hh>
 #include <ustd/Array.hh>
 #include <ustd/Format.hh>
 #include <ustd/Log.hh>
+#include <ustd/Memory.hh>
 #include <ustd/String.hh>
 #include <ustd/StringBuilder.hh>
 #include <ustd/StringView.hh>
 #include <ustd/Types.hh>
-#include <ustd/Vector.hh>
 
 namespace {
 
-usize print_device(const char *name) {
+void print_device(const char *name) {
     core::File file(ustd::format("/dev/pci/{}", name).view());
     if (!file) {
         ustd::printf("lspci: no device {}\n", name);
-        return 1;
+        core::exit(1);
     }
     PciDeviceInfo info{};
     if (auto rc = file.read({&info, sizeof(PciDeviceInfo)}); rc < 0) {
         ustd::printf("lspci: {}: {}\n", name, core::error_string(rc));
-        return 1;
+        core::exit(1);
     }
     ustd::StringBuilder builder;
     builder.append("{}\n", name);
@@ -34,34 +35,20 @@ usize print_device(const char *name) {
         }
     }
     ustd::printf("{}", builder.build().view());
-    return 0;
 }
 
 } // namespace
 
 usize main(usize argc, const char **argv) {
     if (argc == 2) {
-        return print_device(argv[1]);
+        print_device(argv[1]);
     }
-    ssize signed_byte_count = Syscall::invoke(Syscall::read_directory, "/dev/pci", nullptr);
-    if (signed_byte_count < 0) {
-        ustd::printf("lspci: {}\n", core::error_string(signed_byte_count));
+    auto rc = core::iterate_directory("/dev/pci", [](ustd::StringView name) {
+        print_device(name.data());
+    });
+    if (rc < 0) {
+        ustd::printf("lscpi: {}\n", core::error_string(rc));
         return 1;
-    }
-    auto byte_count = static_cast<usize>(signed_byte_count);
-    ustd::LargeVector<char> data;
-    data.ensure_capacity(byte_count);
-    Syscall::invoke(Syscall::read_directory, "/dev/pci", data.data());
-    ustd::Vector<ustd::StringView> views;
-    for (usize byte_offset = 0; byte_offset != byte_count;) {
-        ustd::StringView view(data.data() + byte_offset);
-        views.push(view);
-        byte_offset += view.length() + 1;
-    }
-    for (const auto &view : views) {
-        if (auto rc = print_device(view.data()); rc != 0) {
-            return rc;
-        }
     }
     return 0;
 }
