45dc86b386e23239daccab33d16159c131c94f42
Owen Smith <yeeetari@gmail.com>
2021-12-21 23:03:46 +0000

    kernel: Don't attempt to free non-allocated PhysicalPages

--- kernel/mem/PhysicalPage.cc
+++ kernel/mem/PhysicalPage.cc
@@ -7,22 +7,25 @@
 ustd::SharedPtr<PhysicalPage> PhysicalPage::allocate(PhysicalPageSize size) {
     switch (size) {
     case PhysicalPageSize::Normal:
-        return ustd::make_shared<PhysicalPage>(MemoryManager::alloc_frame(), size);
+        return ustd::make_shared<PhysicalPage>(MemoryManager::alloc_frame(), size, true);
     case PhysicalPageSize::Large: {
         const auto phys = reinterpret_cast<uintptr>(MemoryManager::alloc_contiguous(2_MiB));
-        return ustd::make_shared<PhysicalPage>(phys, size);
+        return ustd::make_shared<PhysicalPage>(phys, size, true);
     }
     case PhysicalPageSize::Huge:
         const auto phys = reinterpret_cast<uintptr>(MemoryManager::alloc_contiguous(1_GiB));
-        return ustd::make_shared<PhysicalPage>(phys, size);
+        return ustd::make_shared<PhysicalPage>(phys, size, true);
     }
 }
 
 ustd::SharedPtr<PhysicalPage> PhysicalPage::create(uintptr phys, PhysicalPageSize size) {
-    return ustd::make_shared<PhysicalPage>(phys, size);
+    return ustd::make_shared<PhysicalPage>(phys, size, false);
 }
 
 PhysicalPage::~PhysicalPage() {
+    if (!allocated()) {
+        return;
+    }
     switch (size()) {
     case PhysicalPageSize::Normal:
         MemoryManager::free_frame(phys());
--- kernel/mem/PhysicalPage.hh
+++ kernel/mem/PhysicalPage.hh
@@ -17,8 +17,10 @@ public:
     static ustd::SharedPtr<PhysicalPage> allocate(PhysicalPageSize size);
     static ustd::SharedPtr<PhysicalPage> create(uintptr phys, PhysicalPageSize size);
 
-    // Maximum physical address size permitted in AMD64 is 52-bits, meaning we can use safely use 8 for the size enum.
-    PhysicalPage(uintptr phys, PhysicalPageSize size) : m_phys(phys | (static_cast<uint64>(size) << 56u)) {}
+    // Maximum physical address size permitted in AMD64 is 52-bits, meaning we can use safely use 8 for the size enum
+    // and allocated flag.
+    PhysicalPage(uintptr phys, PhysicalPageSize size, bool allocated)
+        : m_phys(phys | (static_cast<uint64>(size) << 56u) | (allocated ? (1ul << 63u) : 0u)) {}
     PhysicalPage(const PhysicalPage &) = delete;
     PhysicalPage(PhysicalPage &&) = delete;
     ~PhysicalPage();
@@ -27,5 +29,6 @@ public:
     PhysicalPage &operator=(PhysicalPage &&) = delete;
 
     uintptr phys() const { return m_phys & 0xfffffffffffffful; }
-    PhysicalPageSize size() const { return static_cast<PhysicalPageSize>((m_phys >> 56u) & 0xffu); }
+    PhysicalPageSize size() const { return static_cast<PhysicalPageSize>((m_phys >> 56u) & 0xfu); }
+    bool allocated() const { return (m_phys & (1ul << 63u)) == (1ul << 63u); }
 };
