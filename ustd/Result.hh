c686323db4f8bea363d992afdcf0c46a034300d4
Owen Smith <yeeetari@gmail.com>
2021-12-22 10:57:13 +0000

    ustd: ErrorUnion type for Result

--- ustd/Result.hh
+++ ustd/Result.hh
@@ -4,6 +4,7 @@
 #include <ustd/Assert.hh>
 #include <ustd/Concepts.hh>
 #include <ustd/Memory.hh>
+#include <ustd/Optional.hh>
 #include <ustd/Traits.hh>
 #include <ustd/Utility.hh>
 
@@ -25,6 +26,56 @@
 
 namespace ustd {
 
+class ErrorUnionBase {
+private:
+    usize m_type;
+    usize m_error;
+
+public:
+    ErrorUnionBase() = default;
+    ErrorUnionBase(usize type, usize error) : m_type(type), m_error(error) {}
+
+    usize error() const { return m_error; }
+    usize type() const { return m_type; }
+};
+
+template <unsigned N, typename... Es>
+struct ErrorUnionImpl;
+
+template <unsigned N>
+struct ErrorUnionImpl<N> : ErrorUnionBase {
+    using ErrorUnionBase::ErrorUnionBase;
+};
+
+template <unsigned N, typename E, typename... Es>
+struct ErrorUnionImpl<N, E, Es...> : ErrorUnionImpl<N + 1, Es...> {
+    using ErrorUnionImpl<N + 1, Es...>::ErrorUnionImpl;
+
+    ErrorUnionImpl(E error) : ErrorUnionImpl<N + 1, Es...>(N, static_cast<usize>(error)) {}
+
+    template <SameAs<E>>
+    Optional<E> as() const {
+        return ErrorUnionBase::type() == N ? static_cast<E>(ErrorUnionBase::error()) : Optional<E>();
+    }
+    template <typename F>
+    Optional<F> as() const {
+        return this->ErrorUnionImpl<N + 1, Es...>::template as<F>();
+    }
+};
+
+template <typename... Es>
+struct ErrorUnion : ErrorUnionImpl<0, Es...> {
+    using ErrorUnionImpl<0, Es...>::ErrorUnionImpl;
+
+    template <typename E>
+    static constexpr bool contains() {
+        return (IsSame<E, Es> || ...);
+    }
+};
+
+template <typename E, typename F>
+concept ErrorUnionContains = E::template contains<F>();
+
 template <typename T, typename E>
 class [[nodiscard]] Result {
     union {
@@ -35,6 +86,8 @@ class [[nodiscard]] Result {
 
 public:
     Result(E error) : m_error(error), m_is_error(true) {}
+    template <typename F>
+    Result(F error) requires ErrorUnionContains<E, F> : m_error(error), m_is_error(true) {}
     template <ConvertibleTo<T> U>
     Result(U &&value) : m_is_error(false) {
         new (m_value.data()) T(forward<U>(value));
@@ -71,6 +124,8 @@ class [[nodiscard]] Result<void, E> {
 public:
     Result() = default;
     Result(E error) : m_error(error), m_is_error(true) {}
+    template <typename F>
+    Result(F error) requires ErrorUnionContains<E, F> : m_error(error), m_is_error(true) {}
     Result(const Result &) = delete;
     Result(Result &&) = delete;
     ~Result() = default;
