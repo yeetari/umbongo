b68fbfdd47d79bbdcddf1abe8576d402ba9a42d0
Owen Smith <yeeetari@gmail.com>
2021-12-22 10:26:24 +0000

    ustd: Allow ScopeGuards to be disarmed

--- ustd/ScopeGuard.hh
+++ ustd/ScopeGuard.hh
@@ -7,15 +7,22 @@ namespace ustd {
 template <typename Callback>
 class ScopeGuard {
     const Callback m_callback;
+    bool m_armed{true};
 
 public:
     ScopeGuard(Callback callback) : m_callback(move(callback)) {}
     ScopeGuard(const ScopeGuard &) = delete;
     ScopeGuard(ScopeGuard &&) = delete;
-    ~ScopeGuard() { m_callback(); }
+    ~ScopeGuard() {
+        if (m_armed) {
+            m_callback();
+        }
+    }
 
     ScopeGuard &operator=(const ScopeGuard &) = delete;
     ScopeGuard &operator=(ScopeGuard &&) = delete;
+
+    void disarm() { m_armed = false; }
 };
 
 template <typename Callback>
