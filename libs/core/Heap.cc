2662598ceda44e91801c0566d9dce1f1c83c9c75
Owen Smith <yeeetari@gmail.com>
2021-12-22 10:47:31 +0000

    libs/core: Implement aligned operator new

--- libs/core/Heap.cc
+++ libs/core/Heap.cc
@@ -1,3 +1,4 @@
+#include <ustd/Memory.hh>
 #include <ustd/Numeric.hh>
 #include <ustd/Types.hh>
 
@@ -16,6 +17,17 @@ void *operator new(usize size) {
 
 void *operator new[](usize size) {
     return operator new(size);
+}
+
+void *operator new(usize size, ustd::align_val_t align) {
+    s_pos = ustd::round_up(s_pos, static_cast<usize>(align));
+    auto *ret = reinterpret_cast<void *>(s_pos);
+    s_pos += size;
+    return ret;
+}
+
+void *operator new[](usize size, ustd::align_val_t align) {
+    return operator new(size, align);
 }
 
 void operator delete(void *) noexcept {}
