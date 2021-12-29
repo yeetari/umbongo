0fbf1fc7c66420e1a9d7a0b67fc5a8416e9484a0
Owen Smith <yeeetari@gmail.com>
2021-12-29 15:57:44 +0000

    coreutils: Create a simple syscfg tool for managing system config

---
+++ coreutils/syscfg.cc
@@ -0,0 +1,41 @@
+#include <config/Config.hh>
+#include <core/Print.hh>
+#include <ustd/Algorithm.hh>
+#include <ustd/Optional.hh>
+#include <ustd/StringView.hh>
+#include <ustd/Types.hh>
+#include <ustd/Vector.hh>
+
+usize main(usize argc, const char **argv) {
+    if ((argc != 1 && argc != 2) || (argc == 2 && ustd::StringView(argv[1]) == "--help")) {
+        core::println("Usage: {} [key][=value]", argv[0]);
+        return 0;
+    }
+    if (argc == 1) {
+        for (const auto &value : config::list_all()) {
+            core::println("{}", value);
+        }
+        return 0;
+    }
+    ustd::StringView arg(argv[1]);
+    if (auto dot_index = ustd::index_of(arg, '.')) {
+        auto domain = arg.substr(0, *dot_index);
+        if (auto equal_index = ustd::index_of(arg, '=')) {
+            auto key = arg.substr(*dot_index + 1, *equal_index);
+            auto value = arg.substr(*equal_index + 1);
+            if (!config::update(domain, key, value)) {
+                core::println("syscfg: failed to update config for {}: no key named {}", domain, key);
+            }
+        } else {
+            auto key = arg.substr(*dot_index + 1);
+            if (auto value = config::lookup(domain, key)) {
+                core::println("{}.{}={}", domain, key, *value);
+            } else {
+                core::println("syscfg: no key named {} in config for {}", key, domain);
+            }
+        }
+    } else {
+        core::println("syscfg: {}: not a valid key", argv[1]);
+    }
+    return 0;
+}
