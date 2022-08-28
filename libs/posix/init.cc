435788101afe37c583d914e437ffbd2318bbd31e
Owen Smith <yeeetari@gmail.com>
2022-08-28 21:15:44 +0100

    libs/posix: Initialise a connection to the log server
    This stops posix application log messages (mainly those from libposix
    itself about unimplemented warnings) from being sent to stdout (which
    is used as a fallback), rather than the log server.

--- libs/posix/init.cc
+++ libs/posix/init.cc
@@ -1,5 +1,8 @@
 #include <sys/cdefs.h>
 
+#include <log/Log.hh>
+#include <ustd/Format.hh>
+#include <ustd/String.hh>
 #include <ustd/Types.hh>
 
 __BEGIN_DECLS
@@ -13,6 +16,7 @@ int main(int argc, char **argv);
 __END_DECLS
 
 size_t main(size_t argc, const char **argv) {
+    log::initialise(ustd::format("posix-{}", argv[0]));
     __stdio_init();
     return static_cast<size_t>(main(static_cast<int>(argc), const_cast<char **>(argv)));
 }
