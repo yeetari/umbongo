43242eb210599e4abd95c5e79deeb9198f94a27c
Owen Smith <yeeetari@gmail.com>
2022-09-13 18:26:11 +0100

    ustd: Allow Atomic enums

--- ustd/Atomic.hh
+++ ustd/Atomic.hh
@@ -35,15 +35,29 @@ public:
 };
 
 template <typename T>
-concept IntegralOrPointer = is_integral<T> || is_pointer<T>;
+struct SimpleAtomicStorageType {
+    using type = T;
+};
+template <Enum T>
+struct SimpleAtomicStorageType<T> {
+    using type = underlying_type<T>;
+};
+template <typename T>
+using simple_atomic_storage_t = typename SimpleAtomicStorageType<T>::type;
 
-template <IntegralOrPointer T>
+template <typename T>
+concept SimpleAtomic = requires(simple_atomic_storage_t<T> t) {
+    __atomic_load_n(&t, __ATOMIC_RELAXED);
+};
+
+template <SimpleAtomic T>
 class Atomic<T> {
-    T m_value{};
+    using storage_t = simple_atomic_storage_t<T>;
+    storage_t m_value{};
 
 public:
     Atomic() = default;
-    Atomic(T value) : m_value(value) {}
+    Atomic(T value) : m_value(storage_t(value)) {}
     Atomic(const Atomic &) = delete;
     Atomic(Atomic &&) = delete;
     ~Atomic() = default;
@@ -51,27 +65,40 @@ public:
     Atomic &operator=(const Atomic &) = delete;
     Atomic &operator=(Atomic &&) = delete;
 
-    // TODO: Declare these out of line.
-    bool compare_exchange(T &expected, T desired, MemoryOrder success_order, MemoryOrder failure_order) {
-        return __atomic_compare_exchange_n(&m_value, &expected, desired, false, static_cast<int>(success_order),
-                                           static_cast<int>(failure_order));
+    // TODO: Declare these out of line when clang supports it.
+    bool compare_exchange(T &expected, T desired, MemoryOrder success_order = MemoryOrder::Relaxed,
+                          MemoryOrder failure_order = MemoryOrder::Relaxed) {
+        return __atomic_compare_exchange_n(&m_value, reinterpret_cast<storage_t *>(&expected), storage_t(desired),
+                                           false, static_cast<int>(success_order), static_cast<int>(failure_order));
     }
-    bool compare_exchange_temp(T expected, T desired, MemoryOrder success_order, MemoryOrder failure_order) {
-        return __atomic_compare_exchange_n(&m_value, &expected, desired, false, static_cast<int>(success_order),
-                                           static_cast<int>(failure_order));
-    }
-    T exchange(T desired, MemoryOrder order) volatile {
-        return __atomic_exchange_n(&m_value, desired, static_cast<int>(order));
-    }
-    T load(MemoryOrder order) const volatile { return __atomic_load_n(&m_value, static_cast<int>(order)); }
-    void store(T value, MemoryOrder order) volatile { __atomic_store_n(&m_value, value, static_cast<int>(order)); }
 
-    T fetch_add(T value, MemoryOrder order) volatile {
+    bool compare_exchange_temp(T expected, T desired, MemoryOrder success_order = MemoryOrder::Relaxed,
+                               MemoryOrder failure_order = MemoryOrder::Relaxed) {
+        return __atomic_compare_exchange_n(&m_value, reinterpret_cast<storage_t *>(&expected), storage_t(desired),
+                                           false, static_cast<int>(success_order), static_cast<int>(failure_order));
+    }
+
+    T exchange(T desired, MemoryOrder order = MemoryOrder::Relaxed) volatile {
+        return T(__atomic_exchange_n(&m_value, storage_t(desired), static_cast<int>(order)));
+    }
+
+    T fetch_add(T value, MemoryOrder order = MemoryOrder::Relaxed) volatile {
         return __atomic_fetch_add(&m_value, value, static_cast<int>(order));
     }
-    T fetch_sub(T value, MemoryOrder order) volatile {
+
+    T fetch_sub(T value, MemoryOrder order = MemoryOrder::Relaxed) volatile {
         return __atomic_fetch_sub(&m_value, value, static_cast<int>(order));
     }
+
+    T load(MemoryOrder order = MemoryOrder::Relaxed) const volatile {
+        return T(__atomic_load_n(&m_value, static_cast<int>(order)));
+    }
+
+    void store(T value, MemoryOrder order = MemoryOrder::Relaxed) volatile {
+        __atomic_store_n(&m_value, storage_t(value), static_cast<int>(order));
+    }
+
+    storage_t *raw_ptr() { return &m_value; }
 };
 
 template <typename T>
